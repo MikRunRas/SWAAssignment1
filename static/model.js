5d7ce261bc5cb6b955b7c5ce90ee191e7bb8ca14
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-09-20 18:29:47 +0200

    Attempt at making Getters

--- static/model.js
+++ static/model.js
@@ -1,37 +1,105 @@
-function currentModel(model) {
-    let temperature = model.temperature
-    let percipitation = model.percipitation
-    let wind_speed = model.wind
-    let cloud_coverage = model.cloud
+const Event = (time, place) => {
+    // Fields
+    const _time = time
+    const _place = place
 
+    // Functions
+    const getTime = () => _time
+    const getPlace = () => _place
+
+    // Return Object
     return {
-        temperature,
-        percipitation,
-        wind_speed,
-        cloud_coverage,
-        dump
+        getTime,
+        getPlace
     }
 }
 
-function forecastModel(model) {
-    let temperature = model.temperature
-    let percipitation = model.percipitation
-    let wind_speed = model.wind
-    let cloud_coverage = model.cloud
+const WeatherData = (time, place, value, type, unit) => {
+    // composition?
+    const event = Event(time, place)
 
+    // Fields
+    const _type = type
+    const _value = value
+    const _unit = unit
+
+    // Functions
+    const getType = () => _type
+    const getValue = () => _value
+    const getUnit = () => _unit
+
+    // Return Object
     return {
-        temperature,
-        percipitation,
-        wind_speed,
-        cloud_coverage,
-        dump
+        ...event,
+        getType,
+        getValue,
+        getUnit
     }
 }
 
+const Temperature = (time, place, value, type, unit) => {
+    // ??
+    const weatherData = WeatherData(time, place, value, type, unit)
 
-function dump() {
-    // Using [this.] to ensure the data is the correct data in the object
-    console.log(` ! MODEL DATA DUMP !\n\t${this.temperature}, ${this.percipitation}, ${this.wind_speed}, ${this.cloud_coverage}`)
+    // Functions
+    const convertToC = (farenheit) => -1
+    const convertToF = (celcius) => -1
+
+    // Return Object
+    return {
+        ...weatherData,
+        convertToC,
+        convertToF
+    }
 }
 
-export { currentModel, forecastModel }
+const Precipitation = (time, place, value, type, unit, precipitationType) => {
+    const weatherData = WeatherData(time, place, value, type, unit)
+
+    // Fields
+    const _precipitationType = precipitationType
+
+    // Functions
+    const getPrecipitationType = () => _precipitationType
+    const convertToInches = (mm) => -1
+    const convertToMm = (inches) => -1
+
+    // Return Object
+    return {
+        ...weatherData,
+        getPrecipitationType,
+        convertToInches,
+        convertToMm
+    }
+}
+
+const Wind = (time, place, value, type, unit, direction) => {
+    const weatherData = WeatherData(time, place, value, type, unit)
+
+    // Fields
+    const _direction = direction
+
+    // Functions
+    const getDirection = () => _direction
+    const convertToMPH = (MetersPrSecond) => -1
+    const convertToMS = (MilesPrHour) => -1
+
+    // Return Object
+    return {
+        ...weatherData,
+        getDirection,
+        convertToMPH,
+        convertToMS
+    }
+}
+
+const CloudCoverage = (time, place, value, type, unit) => {
+    const _weatherData = WeatherData(time, place, value, type, unit)
+
+    // Return Object
+    return {
+        ...weatherData
+    }
+}
+
+export { Temperature, Precipitation, Wind, CloudCoverage }
